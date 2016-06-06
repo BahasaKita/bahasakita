a5e3566ab1551708a1d1046bd4c8176d13f66419
nizarakbarmei <nizar.akbar.m@mail.ugm.ac.id>
2016-06-06 09:03:58 +0700

    reverse translating selesai tapi masih belum sempurna

--- translateController.js
+++ translateController.js
@@ -1,21 +1,25 @@
-var key="id";
 // var value=document.getElementById("source").value;
 function reverseTranslate() {
-	if(document.getElementsByTagName("textarea")[0].getAttribute("name")==id) {
-		document.getElementsByTagName("textarea")[0].setAttribute("name")="jw";
-		document.getElementsByTagName("textarea")[1].setAttribute("name")="id";
+//	alert(document.getElementsByTagName("textarea")[0].getAttribute("name")=="id");
+//	alert(document.getElementsByTagName("textarea")[0].getAttribute("name")=="jw");
+	if(document.getElementsByTagName("textarea")[0].getAttribute("name")=="id") {
+		document.getElementsByTagName("textarea")[0].setAttribute("name","jw");
+		document.getElementsByTagName("textarea")[1].setAttribute("name","id");
 		window.key="jw";
+		//alert(window.key);
 	}
-	else if(document.getElementsByTagName("textarea")[0].getAttribute("name")==jw) {
-		document.getElementsByTagName("textarea")[0].setAttribute("name")="id";
-		document.getElementsByTagName("textarea")[1].setAttribute("name")="jw";
+	else if(document.getElementsByTagName("textarea")[0].getAttribute("name")=="jw") {
+		document.getElementsByTagName("textarea")[0].setAttribute("name","id");
+		document.getElementsByTagName("textarea")[1].setAttribute("name","jw");
 		window.key="id";
 	}
 }
 function translate(str) {
 	var url="translate.php";
+	var key=(document.getElementsByTagName("textarea")[0].getAttribute("name")=="id")?"id":"jw";
 	var value=document.getElementById("source").value;
 	var vars=url+"?"+key+"="+value;
+	
 	
 
 	if(str.length==0) {
